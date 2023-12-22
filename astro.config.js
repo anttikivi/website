5046f898ebe425fc8d8b6d7ae33f9e6b90df944d
Antti Kivi <antti.kivi@visiosto.fi>
2023-12-22 02:38:39 +0200

    build: add Astro configuration

---
+++ astro.config.js
@@ -0,0 +1,27 @@
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    format: 'directory',
+  },
+  site:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:4321'
+      : 'https://www.anttikivi.fi',
+  trailingSlash: 'always',
+  integrations: [tailwind(), sitemap()],
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          rel: ['nofollow'],
+        },
+      ],
+    ],
+  },
+});
