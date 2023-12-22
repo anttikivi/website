8b5ed947b26622c4a6669d73c6ab79832ac2a896
Antti Kivi <antti.kivi@visiosto.fi>
2023-12-22 02:37:02 +0200

    build: add Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,31 @@
+/** @type {import('prettier').Config} */
+const config = {
+  plugins: [
+    'prettier-plugin-packagejson',
+    'prettier-plugin-astro',
+    'prettier-plugin-sh',
+    'prettier-plugin-organize-imports',
+    'prettier-plugin-tailwindcss',
+    'prettier-plugin-astro-organize-imports',
+  ],
+  arrowParens: 'always',
+  bracketSpacing: true,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  proseWrap: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
+
+export default config;
